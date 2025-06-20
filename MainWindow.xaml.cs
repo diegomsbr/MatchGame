d0420a4deb2ca06cedea4c5664ae8bed1d29d330
Diego Mendonça Silva <diegomsbr@gmail.com>
2025-06-20 17:33:58 -0300

    adding event MouseDown

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,8 +24,8 @@ namespace MatchGame
 
         private void SetUpGame()
         {
-            List<string> animalEmoji = new List<string>()
-            {
+            List<string> animalEmoji =
+            [
                 "😺","😺",
                 "🐵","🐵",
                 "🦒","🦒",
@@ -34,16 +34,40 @@ namespace MatchGame
                 "🐷","🐷",
                 "🐰","🐰",
                 "🐻","🐻",
-            };
+            ];
 
             Random random = new Random();
 
-            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
                 int index = random.Next(animalEmoji.Count);
                 string nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
+            }
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
             }
         }
     }
